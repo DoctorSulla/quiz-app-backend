8c6a85d15a263fcc5ca16a6395c21b6e86298774
Matthew Halliday <matthew.halliday@gmail.com>
2022-03-13 10:59:35 +0000

    Updated readme and added blank config file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# quiz-app-backend
+This repository contains the backend APIs for QuizApp which is written in PHP. In order to get the project up and running you will need to configure your own config file from config-template.php with AWS / SMTP credentials and a hashing key.
+
+You will also need a DynamoDB with the following tables:
+- activation_codes (Partition key: email)
+- categories (Partition key: category)
+- games (Partition key: id)
+- users (Partition key: email)
---
+++ config-template.php
@@ -0,0 +1,23 @@
+<?php
+//AWS Configs
+$region='eu-west-2';
+$key='';
+$secret='';
+
+// SMTP credentials
+$smtpUser = '';
+$smtpPassword = '';
+
+//Hashing Secret
+$hmacSecret='';
+
+// Default JWT expiry
+$durationInDays = 30;
+$durationInSeconds = $durationInDays * 24 * 3600;
+// Max password retryCount
+$maxPasswordRetries = 5;
+
+// Miscellaneous Config
+$charSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"];
+
+?>
